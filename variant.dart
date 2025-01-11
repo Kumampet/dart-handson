563daded3eabec3815d87a297a07c46b7f4b4e86
Kumampet <kumampet@gmail.com>
2025-01-11 14:46:49 +0900

    アクセスしたオブジェクトを読み取る

---
+++ variant.dart
@@ -0,0 +1,24 @@
+void main() {
+  final String Function(int) age = printAge;
+  print(age(26));
+
+  final String Function() intro = printIntroduction;
+  print(intro());
+}
+
+// 文字列中での変数の使用
+String printAge(int age) {
+  return "Your age is $age.";
+}
+
+// アクセスしたオブジェクトを読み取る
+String printIntroduction() {
+  final Map<String, dynamic> myInfo = {
+    'name': 'Kumamoto',
+    'age': 26,
+    'job': 'Engineer'
+  };
+  final String intro =
+      "My name is ${myInfo['name']}! I'm a ${myInfo['age']} years old. My job is ${myInfo['job']}. Nice to meet you.";
+  return intro;
+}
